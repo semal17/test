7c85929b78565daebde4eb0d6f833e5dcc291814
Semen <senia-2010@mail.ru>
2022-06-23 08:38:01 +0300

    gh

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-let val = document.querySelector('.form__input--range');
-let sum = document.querySelector('.form__item-capt');
-val.addEventListener("change", function(){
-  sum.textContent = `${val.value} %`;
-});
-
-console.log(val.value);
